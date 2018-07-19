4184c9ae120eb4a3bcbda2e461d24e2f86687ad5
betejmihai <betej.mihai@gmail.com>
2018-07-20 02:34:21 +0300

    Outgoing message work

--- iOS/TBD/TBD/Classes/Messages/Views/IncomingMessageCell.swift
+++ iOS/TBD/TBD/Classes/Messages/Views/IncomingMessageCell.swift
@@ -11,6 +11,15 @@ import UIKit
 class IncomingMessageCell: UITableViewCell {
 
     @IBOutlet weak var messageContainer: UIView!
+    @IBOutlet weak var messageLabel: UILabel!
+    @IBOutlet weak var incomingMessageContainer: UIView!
+    @IBOutlet weak var incomingMessageContainerHeightConstraint: NSLayoutConstraint!
+    @IBOutlet weak var incomingMessageContainerLeadingConstraint: NSLayoutConstraint!
+
+    
+    let defaultContainerHeight: CGFloat = 36
+    let defaultContainerTrailing: CGFloat = 48
+
     
     override func awakeFromNib() {
         super.awakeFromNib()
@@ -20,8 +29,17 @@ class IncomingMessageCell: UITableViewCell {
         messageContainer.layer.cornerRadius = 8
     }
     
-    func configure(_ contact: Contact) {
+    func configure(_ message: Message) {
+//        let maxW = bounds.width - defaultContainerTrailing - 10
+//        let maxH = CGFloat.greatestFiniteMagnitude
+//        let requiredMessageLabelSize = (message.content as NSString).boundingRect(with: CGSize(width: maxW - 16, height: maxH), options: [.usesFontLeading, .usesLineFragmentOrigin], attributes: [NSAttributedStringKey.font : messageLabel.font], context: nil)
+//        
+//        incomingMessageContainerHeightConstraint.constant = requiredMessageLabelSize.height + 12
+//        incomingMessageContainerLeadingConstraint.constant = bounds.width - requiredMessageLabelSize.width - 16 - 10
+//        layoutIfNeeded()
         
+        messageLabel.text = message.content
+
     }
 
 }
