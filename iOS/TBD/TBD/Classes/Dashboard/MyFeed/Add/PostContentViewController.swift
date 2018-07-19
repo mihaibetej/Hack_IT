14d65b92543e327aa1eb1b1bd8d760fc057b14f0
Emanuel Jarnea <emanuel.jarnea>
2018-07-19 19:59:10 +0300

    kb avoidance

--- iOS/TBD/TBD/Classes/Dashboard/MyFeed/Add/PostContentViewController.swift
+++ iOS/TBD/TBD/Classes/Dashboard/MyFeed/Add/PostContentViewController.swift
@@ -10,13 +10,56 @@ import UIKit
 
 class PostContentViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var textArea: UITextView!
+    @IBOutlet weak var imageViewBottomConstraint: NSLayoutConstraint!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         textArea.text = nil
-        textArea.becomeFirstResponder()
+//        imageView.image = nil
     }
-
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+//        
+//        textArea.becomeFirstResponder()
+        
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(self.keyboardNotification(notification:)),
+                                               name: NSNotification.Name.UIKeyboardWillChangeFrame,
+                                               object: nil)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self)
+    }
+    
+    @objc func keyboardNotification(notification: NSNotification) {
+        if let userInfo = notification.userInfo {
+            let endFrame = (userInfo[UIKeyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue
+            let endFrameY = endFrame?.origin.y ?? 0
+            let duration:TimeInterval = (userInfo[UIKeyboardAnimationDurationUserInfoKey] as? NSNumber)?.doubleValue ?? 0
+            let animationCurveRawNSN = userInfo[UIKeyboardAnimationCurveUserInfoKey] as? NSNumber
+            let animationCurveRaw = animationCurveRawNSN?.uintValue ?? UIViewAnimationOptions.curveEaseInOut.rawValue
+            let animationCurve:UIViewAnimationOptions = UIViewAnimationOptions(rawValue: animationCurveRaw)
+            if endFrameY >= UIScreen.main.bounds.size.height {
+                self.imageViewBottomConstraint?.constant = 20
+            } else {
+                self.imageViewBottomConstraint?.constant = endFrame!.size.height + 20
+            }
+            UIView.animate(withDuration: duration,
+                           delay: TimeInterval(0),
+                           options: animationCurve,
+                           animations: { self.view.layoutIfNeeded() },
+                           completion: nil)
+        }
+    }
+    
+    @IBAction func didTapOutsideKeyboard(_ sender: UITapGestureRecognizer) {
+        view.endEditing(true)
+    }
 }
